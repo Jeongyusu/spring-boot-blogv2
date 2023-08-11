c5ae0d3e7948192d589ffd9d3b8e234a6e590fe4
Jeongyusu <Jeongyusu@naver.com>
2023-08-11 14:15:57 +0900

    Board서비스의 글쓰기 구현완료

--- src/main/java/shop/mtcoding/blogv2/board/BoardController.java
+++ src/main/java/shop/mtcoding/blogv2/board/BoardController.java
@@ -4,37 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import shop.mtcoding.blogv2.user.User;
 
 @Controller
 public class BoardController {
-
     @Autowired
-    private BoardRepository boardRepository;
+    private BoardService boardService;
 
     @GetMapping("/board/saveForm")
     public String saveForm() {
         return "board/saveForm";
     }
 
-    // 1.데이터 받기
-    // 2.인증체크 (:TODO)
-    // 3.유효성검사(1번 과정이 있을 때) (:TODO)
-    // 4.핵심로직 호출(서비스)
-    // 5.view or data 응답
+    // 1. 데이터 받기 (V)
+    // 2. 인증체크 (:TODO)
+    // 3. 유효성 검사 (:TODO)
+    // 4. 핵심로직 호출(서비스)
+    // 5. view or data 응답
     @PostMapping("/board/save")
     public String save(BoardRequest.SaveDTO saveDTO) {
-        // System.out.println("title : " + saveDTO.getTitle());
-        // System.out.println("content : " + saveDTO.getContent());
-        Board board = Board.builder()
-                .title(saveDTO.getTitle())
-                .content(saveDTO.getContent())
-                .user(User.builder().id(1).build())
-                .build();
-
-        boardRepository.save(board);
+        boardService.글쓰기(saveDTO, 1);
         return "redirect:/";
     }
-
 }
--- src/main/java/shop/mtcoding/blogv2/board/BoardService.java
+++ src/main/java/shop/mtcoding/blogv2/board/BoardService.java
@@ -1,17 +1,22 @@
 package shop.mtcoding.blogv2.board;
 
-import javax.transaction.Transactional;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 import shop.mtcoding.blogv2.user.User;
 
 @Service
 public class BoardService {
+    @Autowired
+    private BoardRepository boardRepository;
 
     @Transactional
-    public void 회원가입(BoardRequest.SaveDTO saveDTO, User sessionUser) {
-
+    public void 글쓰기(BoardRequest.SaveDTO saveDTO, int sessionUserId) {
+        Board board = Board.builder()
+                .title(saveDTO.getTitle())
+                .content(saveDTO.getContent())
+                .user(User.builder().id(sessionUserId).build())
+                .build();
+        boardRepository.save(board);
     }
-
 }
