fc0b9652e869073308facde871288d25b856276e
JeongyusuSP9 <onlys2545@naver.com>
2023-08-14 20:04:03 +0900

    게시글 쓰기 완료

--- src/main/java/shop/mtcoding/blogv2/board/BoardController.java
+++ src/main/java/shop/mtcoding/blogv2/board/BoardController.java
@@ -1,8 +1,27 @@
 package shop.mtcoding.blogv2.board;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/saveForm")
+    public String saveForm(){
+        return "board/saveForm";
+    }
+
+
+    @PostMapping("/board/save")
+    public String save(BoardRequest.SaveDTO saveDTO){
+        boardService.글쓰기(saveDTO, 1);
+        return "redirect:/";
+    }
+
 }
+
--- src/main/java/shop/mtcoding/blogv2/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blogv2/board/BoardRepository.java
@@ -1,8 +1,9 @@
 package shop.mtcoding.blogv2.board;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class BoardRepository {
+
+public interface BoardRepository extends JpaRepository<Board, Integer>{
 
 }
