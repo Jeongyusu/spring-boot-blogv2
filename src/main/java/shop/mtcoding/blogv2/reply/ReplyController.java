a0b5fb06e79b9c451648eb9579b58c015814c178
Jeongyusu <onlys2545@naver.com>
2023-08-21 18:54:15 +0900

    댓글쓰기, 삭제 완료

--- src/main/java/shop/mtcoding/blogv2/reply/ReplyController.java
+++ src/main/java/shop/mtcoding/blogv2/reply/ReplyController.java
@@ -1,8 +1,47 @@
 package shop.mtcoding.blogv2.reply;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import shop.mtcoding.blogv2._core.util.Script;
+import shop.mtcoding.blogv2.board.BoardRepository;
+import shop.mtcoding.blogv2.board.BoardService;
+import shop.mtcoding.blogv2.user.User;
 
 @Controller
 public class ReplyController {
 
+    @Autowired
+    private HttpSession session;
+
+    @Autowired
+    private ReplyService replyService;
+
+    @Autowired
+    private ReplyRepository ReplyRepository;
+
+    @PostMapping("/reply/save")
+    public String save(ReplyRequest.SaveDTO saveDTO) {
+
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        if (sessionUser == null) {
+            return "redirect:/loginForm";
+        }
+
+        replyService.댓글쓰기(saveDTO, sessionUser.getId());
+        return "redirect:/board/" + saveDTO.getBoardId();
+    }
+
+    @PostMapping("/reply/{id}/delete")
+    public @ResponseBody String delete(@PathVariable Integer id, Integer boardId) {
+        // 인증체크
+        replyService.삭제(id);
+        return Script.href("/board/" + boardId);
+
+    }
 }
--- src/main/java/shop/mtcoding/blogv2/reply/ReplyRepository.java
+++ src/main/java/shop/mtcoding/blogv2/reply/ReplyRepository.java
@@ -1,8 +1,15 @@
 package shop.mtcoding.blogv2.reply;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 public interface ReplyRepository extends JpaRepository<Reply, Integer> {
 
+    // @Query("insert into reply_tb(comment, board_id, user_id) values(:comment,
+    // :boardId, :userId)")
+    // Optional<Reply> save
 }
